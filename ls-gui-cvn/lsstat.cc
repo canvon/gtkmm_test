03daa78943c47091e7ca40de52fb7e807bc4b28a
Fabian Pietsch <fabian@canvon.de>
2017-04-14 17:37:32 +0200

    [ls-gui-cvn/lsstat] Extend with real functionality
    That is, add all those accessor methods needed to get at the actual
    information from the stat buffer (without the need to include C headers
    or use macros in the using code).  Or, at least most of them.

--- ls-gui-cvn/lsstat.cc
+++ ls-gui-cvn/lsstat.cc
@@ -32,6 +32,59 @@ LsStat::LsStat(const char *pathname)
 	}
 }
 
+bool LsStat::get_is_reg()  { return S_ISREG (pimpl->sb.st_mode); }
+bool LsStat::get_is_dir()  { return S_ISDIR (pimpl->sb.st_mode); }
+bool LsStat::get_is_chr()  { return S_ISCHR (pimpl->sb.st_mode); }
+bool LsStat::get_is_blk()  { return S_ISBLK (pimpl->sb.st_mode); }
+bool LsStat::get_is_fifo() { return S_ISFIFO(pimpl->sb.st_mode); }
+bool LsStat::get_is_lnk()  { return S_ISLNK (pimpl->sb.st_mode); }
+bool LsStat::get_is_sock() { return S_ISSOCK(pimpl->sb.st_mode); }
+
+bool LsStat::get_filemode_set_uid()  { return (pimpl->sb.st_mode & S_ISUID) == S_ISUID; }
+bool LsStat::get_filemode_set_gid()  { return (pimpl->sb.st_mode & S_ISGID) == S_ISGID; }
+bool LsStat::get_filemode_sticky()   { return (pimpl->sb.st_mode & S_ISVTX) == S_ISVTX; }
+bool LsStat::get_filemode_r_user()   { return (pimpl->sb.st_mode & S_IRUSR) == S_IRUSR; }
+bool LsStat::get_filemode_w_user()   { return (pimpl->sb.st_mode & S_IWUSR) == S_IWUSR; }
+bool LsStat::get_filemode_x_user()   { return (pimpl->sb.st_mode & S_IXUSR) == S_IXUSR; }
+bool LsStat::get_filemode_r_group()  { return (pimpl->sb.st_mode & S_IRGRP) == S_IRGRP; }
+bool LsStat::get_filemode_w_group()  { return (pimpl->sb.st_mode & S_IWGRP) == S_IWGRP; }
+bool LsStat::get_filemode_x_group()  { return (pimpl->sb.st_mode & S_IXGRP) == S_IXGRP; }
+bool LsStat::get_filemode_r_other()  { return (pimpl->sb.st_mode & S_IROTH) == S_IROTH; }
+bool LsStat::get_filemode_w_other()  { return (pimpl->sb.st_mode & S_IWOTH) == S_IWOTH; }
+bool LsStat::get_filemode_x_other()  { return (pimpl->sb.st_mode & S_IXOTH) == S_IXOTH; }
+
+int LsStat::get_filemode_rwx_user()  { return pimpl->sb.st_mode & S_IRWXU; }
+int LsStat::get_filemode_rwx_group() { return pimpl->sb.st_mode & S_IRWXG; }
+int LsStat::get_filemode_rwx_other() { return pimpl->sb.st_mode & S_IRWXO; }
+
+std::string LsStat::get_mode_str()
+{
+	// FIXME
+	return "N.A.";
+}
+
+int LsStat::get_nlink() { return pimpl->sb.st_nlink; }
+
+int LsStat::get_uid()   { return pimpl->sb.st_uid; }
+int LsStat::get_gid()   { return pimpl->sb.st_gid; }
+
+std::string LsStat::get_user()
+{
+	// FIXME
+	return "N.A.";
+}
+
+std::string LsStat::get_group()
+{
+	// FIXME
+	return "N.A.";
+}
+
+long long LsStat::get_size()
+{
+	return pimpl->sb.st_size;
+}
+
 struct stat &LsStat::get_stat()
 {
 	return pimpl->sb;
--- ls-gui-cvn/lsstat.hh
+++ ls-gui-cvn/lsstat.hh
@@ -2,11 +2,45 @@
 #define LS_STAT_HH
 
 #include <memory>
+#include <string>
 
 class LsStat
 {
 public:
 	LsStat(const char *pathname);
+
+	bool get_is_reg(),
+	     get_is_dir(),
+	     get_is_chr(),
+	     get_is_blk(),
+	     get_is_fifo(),
+	     get_is_lnk(),
+	     get_is_sock();
+	bool get_filemode_set_uid(),
+	     get_filemode_set_gid(),
+	     get_filemode_sticky(),
+	     get_filemode_r_user(),
+	     get_filemode_w_user(),
+	     get_filemode_x_user(),
+	     get_filemode_r_group(),
+	     get_filemode_w_group(),
+	     get_filemode_x_group(),
+	     get_filemode_r_other(),
+	     get_filemode_w_other(),
+	     get_filemode_x_other();
+	int get_filemode_rwx_user(),
+	    get_filemode_rwx_group(),
+	    get_filemode_rwx_other();
+	std::string get_mode_str();
+
+	int get_nlink();
+
+	int          get_uid(),  get_gid();
+	std::string  get_user(), get_group();
+
+	long long get_size();
+
+	// TODO: Give access to mtime, ctime, atime.
 
 	struct stat &get_stat();
 
@@ -14,5 +48,8 @@ private:
 	class impl;
 	std::shared_ptr<impl> pimpl;
 };
+
+// TODO: Add LsLstat, which retrieves information about the symbolic link
+//       instead of the symlink target.
 
 #endif  // LS_STAT_HH
