0fee8faa842c9df2597373aa581fbbe7b5179bb4
Fabian Pietsch <fabian@canvon.de>
2017-05-08 12:20:03 +0200

    Add describe-cvn.sh: git describe wrapper which is more selective
    Example:
    
    (1) gtkmm_test$ git describe
        ls-gui-cvn/0.9-17-gafecf13
    
    (2) gtkmm_test$ ./describe-cvn.sh 'ls-gui-cvn/*' ls-gui-cvn/
        ls-gui-cvn/0.9-12-g41792d9
    
    (3) gtkmm_test$ ./describe-cvn.sh 'ls-gui-cvn/*' .
        ls-gui-cvn/0.9-17-gafecf13
    
    * See (1) for what git describe alone would give.
    * Compare (2): Instead of 17 commits, 12 commits are counted
      when limiting to changes in the ls-gui-cvn/ subdirectory.
    * When "limiting" to the current directory (in this case:
      to all paths in the repository?) in (3), it's able to produce
      the same output than git describe itself in (1), in some cases.

---
+++ describe-cvn.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+warn() {
+	echo "$BASENAME: $*" >&2
+}
+
+die() {
+	warn "Fatal: $*"
+	exit 1
+}
+
+BASENAME="${0##*/}"
+
+[ "$#" -eq 2 ] || die "Usage: $BASENAME TAG_MATCH SUBDIR"
+
+TAG_MATCH="$1"; shift
+SUBDIR="$1"; shift
+
+[ "$#" -eq 0 ] || die "Trailing arguments"
+
+DESCRIBE=$(git describe --tags --dirty --match="$TAG_MATCH") || die "git describe failed"
+
+[ -n "$DESCRIBE" ] || die "git describe gave empty result"
+
+if grep -q -- '-[0-9]' <<<"$DESCRIBE"
+then
+	TAG=$(sed -e 's/-[0-9].*//' <<<"$DESCRIBE") || die "Cannot get tag name from git describe output: sed failed"
+	SUBDIR_COMMIT_COUNT=$(git rev-list --count "$TAG".. "$SUBDIR") || die "Cannot get count of commits since tag \"$TAG\" in subdir \"$SUBDIR\": git rev-list failed"
+	[ -n "$SUBDIR_COMMIT_COUNT" ] || die "Cannot get count of commits since tag \"$TAG\" in subdir \"$SUBDIR\": git rev-list gave empty result"
+	if [ "$SUBDIR_COMMIT_COUNT" -eq 0 ]
+	then
+		DESCRIBE="$TAG"
+	else
+		COMMIT_HASH=$(git rev-list --max-count=1 --abbrev-commit "$TAG".. "$SUBDIR") || die "Cannot get most recent relevant commit's hash: git rev-list failed"
+		[ -n "$COMMIT_HASH" ] || die "Cannot get most recent relevant commit's hash: git rev-list gave empty result"
+		DESCRIBE="${TAG}-${SUBDIR_COMMIT_COUNT}-g${COMMIT_HASH}"
+	fi
+
+	# TODO: Consider -dirty; limit it to SUBDIR, too?
+fi
+
+echo "$DESCRIBE"
